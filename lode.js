6ee7c11d6b73e80c96b9d37c3b7faf2bebd1c582
Randall Leeds <randall.leeds@gmail.com>
2010-02-06 18:58:15 -0800

    some more skeletal stuff and stub responses

--- lode.js
+++ lode.js
@@ -30,9 +30,14 @@ function handle_request(client_request, client_response) {
 	return;
   }
 
+  //This should probably turn into something like couch's *_handlers conf
   switch(path_parts.length) {
-	case 1: //db request
+	case 1: //root space request (_all_tasks and the like)
+	  handle_basic_request(client_request, client_response, path_parts);
+	  break;
+	case 2: //db request
 	  handle_db_request(client_request, client_response, path_parts);
+	  break;
 	default:
 	  client_response.sendHeader(200, {"Content-Type": "text/plain"});
 	  client_response.sendBody("Check back soon...");
@@ -40,9 +45,24 @@ function handle_request(client_request, client_response) {
   }
 };
 
+function handle_basic_request(client_request, client_response, path_parts) {
+  client_response.sendHeader(200, {"Content-Type": "text/plain"});
+  client_response.sendBody("Nothing here yet");
+  client_response.finish();
+};
+
 function handle_db_request(client_request, client_response, path_parts) {
   db_name = path_parts[0];
   client_response.sendHeader(200, {"Content-Type": "text/plain"});
+  client_response.sendBody("Nothing here yet, but here are the replica uris by shard\n");
+  shards.shards(db_name).forEach(
+	function(repls, shard_no) {
+	  client_response.sendBody("Shard " + shard_no + "\n");
+	  repls.forEach(
+		function(uri) {
+		  client_response.sendBody("\t" + uri + "\n");
+		});
+	});
   client_response.finish();
 }
 
