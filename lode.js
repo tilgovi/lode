5f61f40e53cbe05ac1254a8fed978577a4d8d67e
Randall Leeds <randall.leeds@gmail.com>
2010-02-06 18:15:34 -0800

    add shards.js utility module

--- lode.js
+++ lode.js
@@ -13,18 +13,11 @@
 var sys = require("sys"),
   http = require('http'),
   url = require('url'),
-  crc = require('./crc32');
+  crc = require('./crc32'),
+  shards = require('./shards');
 
 var version = "0.0.1";
 var debug = true;
-
-var shard_map = {
-  'nodes': ['http://localhost:5984/', 'http://localhost:5984/'],
-  'shards': [[0,1], [1,0]]
-};
-
-//dbname -> dbname$0, etc. Change for loop-back replication resharding.
-var shard_index_sep = '$';
 
 function handle_request(client_request, client_response) {
   request_url_parsed = url.parse(client_request.url, true);
---
+++ shards.js
@@ -0,0 +1,29 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+var shard_map = {
+  'nodes': ['http://localhost:5984/', 'http://localhost:5984/'],
+  'shards': [[0,1], [1,0]]
+};
+
+//dbname -> dbname$0, etc. Change for loop-back replication resharding.
+var shard_index_sep = '$';
+
+exports.shards = function(db_name) {
+  return shard_map['shards'].map(
+	function(repls, shard_no) {
+	  return repls.map(
+		function(node_idx) {
+		  return shard_map['nodes'][node_idx] +	[db_name, shard_no].join(shard_index_sep);
+		});
+	  });
+};
