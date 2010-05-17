d052e1ed3aba90c59962bd4f4d78bf5a98c02daa
Robert Newson <robert.newson@gmail.com>
2010-05-17 23:05:00 +0800

    always use binary in proxy.js. but why doesn't this default to Buffer everywhere? wtf.

--- src/proxy.js
+++ src/proxy.js
@@ -3,17 +3,18 @@ var http = require('http');
 exports.proxy = function(proxy_port, proxy_host, client_req, client_resp) {
     var proxy_req = http.createClient(proxy_port, proxy_host).request(
 	client_req.method, client_req.url, client_req.headers);
+    client_req.setEncoding('binary');
     proxy_req.addListener('response', function (proxy_resp) {
 	    client_resp.writeHeader(proxy_resp.statusCode, proxy_resp.headers);
 	    proxy_resp.addListener('data', function (chunk) {
-		    client_resp.write(chunk);
+		    client_resp.write(chunk, 'binary');
 		});
 	    proxy_resp.addListener('end', function () {
 		    client_resp.end();
 		});
 	});
     client_req.addListener('data', function (chunk) {
-	    proxy_req.write(chunk);
+	    proxy_req.write(chunk, 'binary');
 	});
     client_req.addListener('end', function() {
 	    proxy_req.end();
