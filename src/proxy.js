977b87be2bd6d05ee626e09b5ec7faff0edf888b
Robert Newson <robert.newson@gmail.com>
2010-05-18 02:00:19 +0800

    Revert "more encoding settings." because it's stupid.
    This reverts commit ccb99882a11a9edd301de8c6e42bf9700e6f8bf5.

--- src/proxy.js
+++ src/proxy.js
@@ -4,7 +4,6 @@ exports.proxy = function(proxy_port, proxy_host, client_req, client_resp) {
     var proxy_req = http.createClient(proxy_port, proxy_host).request(
 	client_req.method, client_req.url, client_req.headers);
     client_req.setEncoding('binary');
-    proxy_req.setEncoding('binary');
     proxy_req.addListener('response', function (proxy_resp) {
 	    client_resp.writeHeader(proxy_resp.statusCode, proxy_resp.headers);
 	    proxy_resp.addListener('data', function (chunk) {
